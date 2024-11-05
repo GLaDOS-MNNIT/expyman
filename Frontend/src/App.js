bf0d7b12f830f575857de6cad38590b9bbfd53b2
akash6299625764 <bestakashchand@gmail.com>
2024-11-05 13:25:13 +0530

    added react route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import Login from "./components/Login/Login";
-// import SignUp from "./components/SignUp/SignUp";
-// import MainPage from "./components/MainPage";
+import "./App.css";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Home from "./components/MainPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./utils/PrivateRoute";
+// import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <>
-      <Login />
-      {/* <SignUp /> */}
-      {/* <MainPage /> */}
-    </>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/signup/' element={<SignUp/>} />
+          <Route path='/login/' element={<Login/>} />
+          {/* <Route path='/dashboard' element={<PrivateRoute><Dashboard/></PrivateRoute>}/> */}
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
